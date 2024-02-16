66d45c0e5d5a12198be5090370a8cfd7f2d9d922
WaelTarabishi <waeltarabishi222@gmail.com>
2024-02-17 00:04:06 +0200

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import color from "colors";
+import dotenv from "dotenv";
 const app = express();
-
+import connectDB from "./config/db.js";
+dotenv.config();
+connectDB();
 app.listen(3000, () => {
   console.log("runnign in server 3000");
 });
