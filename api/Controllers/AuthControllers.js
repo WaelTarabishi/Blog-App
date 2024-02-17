7fe5512117de591575f86b6542b1601df10c2471
WaelTarabishi <waeltarabishi222@gmail.com>
2024-02-17 15:03:23 +0200

    add   a middleware and a function to hadlerros

--- api/Controllers/AuthControllers.js
+++ api/Controllers/AuthControllers.js
@@ -1,7 +1,7 @@
 import User from "../Models/User.Model.js";
 import bcryptjs from "bcryptjs";
-import {} from "module";
-const singup = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+const singup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (
     !username ||
@@ -11,19 +11,20 @@ const singup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    res.status(400).json({ message: "All fields are required" });
-  }
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-  try {
-    const newUser = new User({
-      username,
-      email,
-      password: hashedPassword,
-    });
-    await newUser.save();
-    res.json({ message: "user Register" });
-  } catch (err) {
-    res.status(501).json({ message: "already exists" });
+    next(errorHandler(500, "All fields are required"));
+  } else {
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    try {
+      const newUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.json({ message: "user Register" });
+    } catch (err) {
+      next(err);
+    }
   }
 };
 
--- api/index.js
+++ api/index.js
@@ -11,8 +11,18 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const port = process.env.Port;
-app.use("/api/users", UserRoutes);
-app.use("/api/auth", authRoute);
 app.listen(port, () => {
   console.log(`Server run's in Port ${port}`);
+});
+app.use("/api/users", UserRoutes);
+app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
