b2b2842620d7806dc15fe9b79fc5b7328431c66c
WaelTarabishi <waeltarabishi222@gmail.com>
2024-03-08 21:47:30 +0200

    add pre installation

--- api/index.js
+++ api/index.js
@@ -8,9 +8,10 @@ import UserRoute from "./Routes/UserRoutes.js";
 import PostRoute from "./Routes/PostRoutes.js";
 import CommentRoute from "./Routes/CommentRoutes.js";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 dotenv.config();
 connectDB();
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -24,6 +25,10 @@ app.use("/api/user", UserRoute);
 app.use("/api/post", PostRoute);
 app.use("/api/comment", CommentRoute);
 
+app.use(express.static(path.join(__dirname, "/clinet/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
